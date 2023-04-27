2a342c836327b6bdc358cc93d3aaa85ea48fd331
saber <saber.tabatabaee@gmail.com>
2023-04-28 00:11:31 +0330

    fr

--- config/translation.php
+++ config/translation.php
@@ -3,8 +3,7 @@
 return [
     'key' => 'ce236df79c674c36b4cce3ea3d01f2fc',
     'source_locale' => 'en',
-    'target_locales' => ['fa'],
-
+    'target_locales' => ['fr', 'fa'],
     /* Directories to scan for Gettext strings */
     'gettext_parse_paths' => ['app', 'resources']
 ];
