e518d239323605c939ed0308cdee7cf08c4853b1
saber <saber.tabatabaee@gmail.com>
2023-05-12 21:11:13 +0330

    soft delete on post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,12 +10,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory, Likeable;
-
+    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
