2dc21aef0db4877755980e65db65abb059a6f775
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-05-13 11:59:52 +0330

    add category into rss like virgool.io

--- resources/views/posts_feed/_show.blade.php
+++ resources/views/posts_feed/_show.blade.php
@@ -4,5 +4,6 @@
     <guid>{{ route('posts.show', $post) }}</guid>
     <pubDate>{{ $post->posted_at->format('r') }}</pubDate>
     <author>{{ $post->author->email }} ({{ $post->author->fullname }})</author>
+    <category>{{ $post->author->email }} ({{ $post->author->fullname }})</category>
     <description>{{ $post->content }}</description>
 </item>
