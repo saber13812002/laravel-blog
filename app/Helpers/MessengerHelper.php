adf75116b50bddf0b22ae26772ac576faa7388ed
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-05-15 20:08:51 +0330

    تست روبات های بله تلگرام ایتا

--- app/Helpers/MessengerHelper.php
+++ app/Helpers/MessengerHelper.php
@@ -11,16 +11,18 @@ class MessengerHelper
     {
         $messenger = Messenger::whereUserId($author_id)->get()->first();
 
-        if ($messenger->bale_bot_token && $messenger->bale_channel_chat_id) {
-            BotHelper::sendMessage($message,$messenger->bale_bot_token, $messenger->bale_channel_chat_id, 'bale');
-        }
+        if ($messenger) {
+            if ($messenger->bale_bot_token && $messenger->bale_channel_chat_id) {
+                BotHelper::sendMessage($message, $messenger->bale_bot_token, $messenger->bale_channel_chat_id, 'bale');
+            }
 
-        if ($messenger->telegram_bot_token && $messenger->telegram_channel_chat_id) {
-            BotHelper::sendMessage($message,$messenger->telegram_bot_token, $messenger->telegram_channel_chat_id, 'telegram');
-        }
+            if ($messenger->telegram_bot_token && $messenger->telegram_channel_chat_id) {
+                BotHelper::sendMessage(strip_tags($message), $messenger->telegram_bot_token, $messenger->telegram_channel_chat_id, 'telegram');
+            }
 
-        if ($messenger->eitaa_bot_token && $messenger->eitaa_channel_chat_id) {
-            BotHelper::sendMessageEitaa($message,$messenger->eitaa_bot_token, $messenger->eitaa_channel_chat_id, 'eitaa');
+            if ($messenger->eitaa_bot_token && $messenger->eitaa_channel_chat_id) {
+                BotHelper::sendMessageEitaa(strip_tags($message), $messenger->eitaa_bot_token, $messenger->eitaa_channel_chat_id, 'eitaa');
+            }
         }
     }
 }
