94dbff86c65e4795bc0763b61e5a1270370c9df8
saber <saber.tabatabaee@gmail.com>
2023-06-06 23:28:29 +0330

    blog bot v5.91 set schedule with command that send message to those content providers that more than one day they have not any posts

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UserPostAlertCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -27,6 +28,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('backup:clean')->daily()->at('01:00');
         $schedule->command('backup:run')->daily()->at('02:00');
         $schedule->command('telescope:prune')->daily()->at('03:00');
+        $schedule->command(UserPostAlertCommand::class)->dailyAt("14:29"); //17:59 iran
     }
 
     /**
