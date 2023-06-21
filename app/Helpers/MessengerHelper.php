4f4f4152f992fa3eb0176c4cb1846e24ce51b27c
saber <saber.tabatabaee@gmail.com>
2023-06-21 22:50:09 +0330

    run command after messenger send call method

--- app/Helpers/MessengerHelper.php
+++ app/Helpers/MessengerHelper.php
@@ -4,6 +4,7 @@ namespace App\Helpers;
 
 use App\Jobs\MessengerSenderJob;
 use App\Models\Messenger;
+use Illuminate\Support\Facades\Artisan;
 
 class MessengerHelper
 {
@@ -19,18 +20,20 @@ class MessengerHelper
 
             if ($messenger->bale_bot_token && $messenger->bale_channel_chat_id) {
                 MessengerSenderJob::dispatch($messageTelegram, $messenger, 'bale')
-                    ->delay(now()->addSecond(2));;
+                    ->delay(now()->addSecond(2));
             }
 
             if ($messenger->telegram_bot_token && $messenger->telegram_channel_chat_id) {
                 MessengerSenderJob::dispatch($messageTelegram, $messenger, 'telegram')
-                    ->delay(now()->addSecond(6));;
+                    ->delay(now()->addSecond(6));
             }
 
             if ($messenger->eitaa_bot_token && $messenger->eitaa_channel_chat_id) {
                 MessengerSenderJob::dispatch($messageEitaa, $messenger, 'eitaa')
-                    ->delay(now()->addSecond(4));;
+                    ->delay(now()->addSecond(4));
             }
+
+            Artisan::call('queue:work');
         }
     }
 }
